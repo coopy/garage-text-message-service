a3fdd63cc6c6de7107dc2c3c6eb320128ab0aa59
Per Nilsson <per.nilsson@formidablelabs.com>
2015-11-27 22:26:51 -0800

    Hook up twilio service to door events

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const log = require('./lib/log');
-const GarageDoorService = require('./lib/garage-door-service');
+'use strict';
 
-GarageDoorService.startListening();
+const log = require('./lib/log').child({ service: 'garage-door-text-message' });
+const GarageDoorService = require('./lib/garage-door-service');
+const TextMessageService = require('./lib/text-message-service');
+
+GarageDoorService.startListening(function (err, socket) {
+  if (err) {
+    throw err;
+  }
+
+  const openMessage = 'Garage door is open';
+  const closeMessage = 'Garage door is closed';
+
+  socket.on('open', function (status) {
+    log.info(status, 'open event received');
+    TextMessageService.sendMessage(openMessage, function (err) {
+      if (err) {
+        return log.error(err);
+      }
+      log.info({ message: openMessage }, 'text message sent');
+    });
+  });
+
+  socket.on('close', function (status) {
+    log.info(status, 'close event received');
+    TextMessageService.sendMessage(closeMessage, function (err) {
+      if (err) {
+        return log.error(err);
+      }
+      log.info({ message: closeMessage }, 'text message sent');
+    });
+  });
+});
