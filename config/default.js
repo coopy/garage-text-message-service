708aefc6e21df54130c0ebb3cfdc44283ebfbcf5
Per Nilsson <per.nilsson@formidablelabs.com>
2015-11-27 22:01:07 -0800

    Initial commit

---
+++ config/default.js
@@ -0,0 +1,7 @@
+module.exports = {
+  services: {
+    garageDoor: {
+      uri: 'ws://10.0.1.6:5001'
+    }
+  }
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+const log = require('./lib/log');
+const GarageDoorService = require('./lib/garage-door-service');
+
+GarageDoorService.startListening();
